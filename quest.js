0b04981a79beca94b93c36d2281bba8435adc9eb
Анжелика Стрижич <ao.stryzhych@lab.vebtech.by>
2020-01-31 13:25:03 +0300

    Add link to quest page

---
+++ quest.js
@@ -0,0 +1,14 @@
+class Quest {
+    constructor(countOfQuestions, questions) {
+        this.questions = questions;
+        this.countOfQuestions = countOfQuestions;
+    }
+}
+
+
+
+function confirmAnswer() {
+    let answer = document.getElementById('answer').value;
+    answer 
+
+}
