ef20362da5848b5a171c6ab578752cc1ea95dcb6
Анжелика Стрижич <ao.stryzhych@lab.vebtech.by>
2020-01-31 15:00:19 +0300

    Remove dependency on expected literals count

--- main.js
+++ main.js
@@ -10,54 +10,29 @@ function check() {
         .split(new RegExp('\([^()]*\)'))
         .filter(value => value && value !== ")" && value !== "(" && value !== "&");
 
-    let countOfGroups = groups.length;
-    let expectedCountOfLiteralsPerGroup = countOfGroups - 1;
-
-    let totalLiterals = [];
-    let totalLiteralsCount = 0;
     let literalGroups = [];
 
     groups.forEach(value => {
         let literals = value.split('|').filter(value => value && value !== "|");
 
-        totalLiterals.push(...literals);
         literalGroups.push(literals);
-
-        totalLiteralsCount += literals.length;
     });
 
     for (i = 0; i < literalGroups.length - 1; i++) {
         for (j = i + 1; j < literalGroups.length; j++) {
+            if (literalGroups[i].length !== literalGroups[j].length) {
+                alert("Not all of subgroups have equal count of variables");
+                return;
+            }
+            
             if (compareArrays(literalGroups[i], literalGroups[j])) {
                 alert("Formula contains equal elementary disjunctions");
                 return;
             }
         }
-
-        if (new Set(literalGroups[i]).size !== expectedCountOfLiteralsPerGroup) {
-            alert("Subgroup N" + i + " contains extra/less/equal propositional vaiables");
-            return;
-        }
     }
 
-    if (totalLiteralsCount / countOfGroups != expectedCountOfLiteralsPerGroup) {
-        alert("Formula is not valid (contains syntax errors) or has extra/less subgroups or literals in subgroups");
-        return;
-    }
-
-    let uniqueLiterals = [];
-
-    totalLiterals.forEach(value => {
-        if (uniqueLiterals.indexOf(value.replace('!', "")) === -1) {
-            uniqueLiterals.push(value);
-        }
-    });
-
-    if (uniqueLiterals.length == expectedCountOfLiteralsPerGroup) {
-        alert("This formula is in principal conjunctive normal form");
-    } else {
-        alert("This formula is not in principal conjunctive normal form");        
-    }
+    alert("This function is in principal conjuctive normal form");
 }
 
 function compareArrays(array1, array2) {
