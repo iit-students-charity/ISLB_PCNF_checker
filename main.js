3c327c483934202c1fa36e359b5a6752effa5e41
River Endless <myowngollum@gmail.com>
2020-02-02 02:40:39 +0300

    Move quest.js to main.js

--- main.js
+++ main.js
@@ -44,3 +44,103 @@ function compareArrays(array1, array2) {
 
     return true;
 }
+
+
+///////////// quest.js
+
+class Quest {
+    constructor(countOfQuestions) {
+        this.questions = generateQuestions(countOfQuestions);
+    }
+
+    addQuestion(question) {
+        this.questions.push(question);
+    }
+}
+
+class Question {
+    constructor(question, answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+}
+
+var quest = new Quest(getRandomInt(10));
+var currentQuestion = new Question();
+var currentQuestionIndex = 0;
+
+var form = document.getElementById("answer");
+
+form.addEventListener("submit", function(event) {
+  next();
+  event.preventDefault();
+}, false);
+
+
+function nextQuestion() {
+    currentQuestion = quest.questions[++currentQuestionIndex];
+}
+
+function next() {
+    highlightAnswer();
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    let answer = document.getElementById('answer').value;
+    if (answer !== currentQuestion.answer) {
+        highlightError();
+    }
+    
+    nextQuestion();
+    refreshAnswers();
+}
+
+function generateQuestions(countOfQuestions) {
+    let countOfGroups;
+    let countOfArgs;
+    let isConjuctive;
+
+    for (i = 0; i < countOfQuestions; i++) {
+        countOfGroups = getRandomInt(5);
+        countOfArgs = getRandomInt(4);
+        isConjuctive = (Math.random() >= 0.5);
+
+        formula = generateFormula(countOfGroups, countOfArgs, isConjuctive);
+        answer = check(formula);
+
+        var question = new Question(formula, answer);
+        quest.addQuestion(question);
+        renderQuestion(question);
+    }
+    
+    return questions;
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max)) + 1;
+}
+
+function generateFormula(countOfGroups, countOfArgs, isConjuctive) {
+    // npm install randexp
+    // generate formula based on regex & input parameters
+    let formula = "adfsdasdad";
+
+    return formula;
+}
+
+// both of ans & que must be rendered on the page start
+function renderQuestion(question) {
+    // add dom elements
+}
+
+function refreshAnswers() {
+    // once render, on next() update radiobuttons state
+}
+
+function highlightAnswer() {
+    let answer = document.getElementById(currentQuestion.answer.toString()).value;
+    
+}
+
+function highlightError() {
+
+}
