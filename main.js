a9d72d1f325dd2741cf6dde1c9b3ac7706ff0b6b
River Endless <myowngollum@gmail.com>
2020-02-05 09:29:41 +0300

    Add test generating module

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
+var checkingMessages = [ 
+    "this function is in principal conjuctive normal form",
+    "symbols must be from A to Z", 
+    "formula has groups divided by '|'", 
+    "not all of subgroups have equal count of variables", 
+    "formula contains equal elementary disjunctions"];
+
 function check(formula) {
     if (formula.match(new RegExp('[^A-Z()|&!]'))) {
-        alert("symbols must be from A to Z");
-        return;
+        return 1;
     }
 
-    if (!formula.match(new RegExp('^(\((!?[A-Z](\|(!?[A-Z]))*)\))(\&(\((!?[A-Z](\|(!?[A-Z]))*)\)))$'))) {
-        alert("invalid syntax");
-        return;
-    }
+    // if (!formula.match(new RegExp('^(\((!?[A-Z](\|(!?[A-Z]))*)\))(\&(\((!?[A-Z](\|(!?[A-Z]))*)\)))*$'))) {
+    //     alert("invalid syntax");
+    //     return;
+    // }
 
     let groups = formula
         .split(new RegExp('\([^()]*\)'))
         .filter(value => value && value !== ")" && value !== "(" && value !== "&");
+
+    if (groups.indexOf('|') !== -1) {
+        return 2;
+    }
 
     let literalGroups = [];
 
@@ -24,18 +34,21 @@ function check(formula) {
     for (i = 0; i < literalGroups.length - 1; i++) {
         for (j = i + 1; j < literalGroups.length; j++) {
             if (literalGroups[i].length !== literalGroups[j].length) {
-                alert("not all of subgroups have equal count of variables");
-                return;
+                return 3;
             }
             
             if (compareArrays(literalGroups[i], literalGroups[j])) {
-                alert("formula contains equal elementary disjunctions");
-                return;
+                return 4;
             }
         }
     }
 
-    alert("this function is in principal conjuctive normal form");
+    return 0;
+}
+
+function check() {
+    let messageCode = check(document.getElementById('formula').value);
+    alert(checkingMessages[messageCode]);
 }
 
 function compareArrays(array1, array2) {
@@ -59,9 +72,11 @@ class Question {
     }
 }
 
-var currentQuestion = new Question(null, false);
+var variablesCodes = [ 'A', 'B', 'C', 'D' ];
+
+var currentQuestion = generateQuestion();
 var countOfQuestions = getRandomInt(10);
-var currentQuestionIndex = 0;
+var currentQuestionIndex = 1;
 
 renderQuestion();
 refreshAnswers();
@@ -90,11 +105,10 @@ function next() {
 }
 
 function generateQuestion() {
-    let countOfGroups = getRandomInt(5);
-    let countOfArgs = getRandomInt(4);
-    let isConjuctive = (Math.random() >= 0.5);
+    let countOfArgs = getRandomInt(3);
+    let countOfGroups = getRandomInt(Math.pow(2, countOfArgs));
 
-    let formula = generateFormula(countOfGroups, countOfArgs, isConjuctive);
+    let formula = generateFormula(countOfGroups, countOfArgs);
     let answer = check(formula);
 
     return new Question(formula, answer);
@@ -104,10 +118,31 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max)) + 1;
 }
 
-function generateFormula(countOfGroups, countOfArgs, isConjuctive) {
-    // npm install randexp
-    // generate formula based on regex & input parameters
-    let formula = "ASD";
+function generateFormula(countOfGroups, countOfArgs) {
+    let formula = '';
+
+    console.log("groups, args " + countOfGroups + " " + countOfArgs);
+
+    for (i = 0; i < countOfGroups; i++) {
+        let group = '(';
+        console.log(i);
+
+        let countOfArgsInParticualrGroup = countOfArgs - getRandomInt(countOfArgs) + 1;
+        for (j = 0; j < countOfArgsInParticualrGroup; j++) {
+            let isNegative = (Math.random() >= 0.5);
+            group += (isNegative ? '!' : '') + variablesCodes[j];
+            if (j < countOfArgsInParticualrGroup - 1) {
+                group += ((Math.random() >= 0.1) ? '|' : '&');
+            }
+        }
+
+        group += ')';
+        formula += group;
+        if (i < countOfGroups - 1) {
+            formula += ((Math.random() >= 0.1) ? '&' : '|');
+        }
+    }
+    console.log(formula);
 
     return formula;
 }
